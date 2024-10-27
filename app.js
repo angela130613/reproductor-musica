80c1b5907b098b3f2ad91ab21d890dc8a35b8d31
Angela Yepes <angelyep76@gmail.com>
2024-10-26 21:41:21 -0500

    song2

--- app.js
+++ app.js
@@ -4,5 +4,12 @@ const playList = [
         artist: 'BTS',
         img: 'images/AIRPLANE PT2.jpg ',
         song:'audio/BTS (防弾少年団) _Airplane pt.2 -Japanese ver.-_ Official MV(MP3_128K)_1.mp3'
-    }
+    },
+    {
+        title:'ALL IN',
+        artist:'Stary Kids',
+        img:'images/ALL IN.jpeg',
+        song:'audio/STRAY KIDS - _ALL IN_ Lyrics [Color Coded_Kan_Rom_Eng](MP3_128K).mp3'
+    },
+   
 ]

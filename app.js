8577e173105751eb5dbe86b637002c190c65684b
Angela Yepes <angelyep76@gmail.com>
2024-10-26 21:39:20 -0500

    song1

---
+++ app.js
@@ -0,0 +1,8 @@
+const playList = [
+    {
+        title: 'AirplanePt2',
+        artist: 'BTS',
+        img: 'images/AIRPLANE PT2.jpg ',
+        song:'audio/BTS (防弾少年団) _Airplane pt.2 -Japanese ver.-_ Official MV(MP3_128K)_1.mp3'
+    }
+]
